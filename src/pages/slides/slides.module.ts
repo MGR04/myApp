804641c1089b41be7b0a0e0a4fb702f90c9af624
Stephania <stephaniaverduzco@gmail.com>
2018-07-31 14:51:12 -0700

    ss

--- src/pages/slides/slides.module.ts
+++ src/pages/slides/slides.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SlidesPage } from './slides';
 
+
 @NgModule({
   declarations: [
     SlidesPage,
--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { HomePage } from '../home/home';
-/**
- * Generated class for the SlidesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
